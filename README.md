6a756fd3fea4998411c388abff2cd3ee6b017607
Gaëlle Richer <gaelle.richer@inria.fr>
2022-12-18 17:31:30 +0100

    Check inputs

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 https://adventofcode.com/2022/
 
-Using different languages (trying out Rust or other to learn, falling back to Python when lazy)
+Original goal: use different and new languages
+Actual achievement: fall back to Python.
 
-```
+## Required
+Place input files at the same level as Python script files
+
+## Run
+Run all days with:
+```cmd
 ./run.sh
 ```
-
+Or a single day with:
+```cmd
+./run.sh <day>
+```
--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-python day_1_python/main.py
-python day_2_python/main.py
-python day_3_python/main.py
-python day_4_python/main.py
+function check_input() {
+    if [ ! -f day_$1/input ]; then
+        echo "[Day ${1}] Please download your input file and save it to 'day_$1/input'"
+        return 1
+    fi
+}
+
+if [ "$#" -ne 1 ]; then
+    check_input "1" && python day_1/main.py
+    check_input "2" && python day_2/main.py
+    check_input "3" && python day_3/main.py
+    check_input "4" && python day_4/main.py
+    check_input "5" && python day_5/main.py
+    check_input "6" && python day_6/main.py
+else
+    check_input $1 && python day_$1/main.py
+fi
